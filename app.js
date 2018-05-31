8e643bf82419bca9b65bb570b586ac5aa744bf37
SML <dltkdals2202@gmail.com>
2018-05-31 20:47:39 +0900

    feat: add swagger-ui module
    add swagger-ui module

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+
 var app = require('express')();
 module.exports = app; // for testing
 
@@ -20,4 +22,16 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
+});
+
+
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+
+    // add swagger-ui (/docs)
+    app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
+    // install middleware
+    swaggerExpress.register(app);
+
 });
